f76b43faa7a1327b197fc3dbfafb2d0a1728b865
Thaddeus Sobotowski <tsobotowski@gmail.com>
2024-10-04 11:36:22 -0400

    modified:   evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,7 @@
 import trec
 import LlamaDetection
+import sys
+import pandas as pd
 
 #Returns dictionary of {doc_id: rating} for all texts in a .trec file
 def trecEval(path):
@@ -8,8 +10,11 @@ def trecEval(path):
     results = texts['doc_id']
     for index, row in texts.iterrows():
         results[row['doc_id']] = LlamaDetection.rate(row['text'])
+    df = pd.DataFrame(results)
+    df.to_csv('trec_eval_results.csv')
     return(results)
     
 path = 'C:/Users/Thad/TextMiningProject3/eRisk2023_T1/new_data/test.trec'
+path=sys.argv[1]
 test = trecEval(path)
 print(test)
